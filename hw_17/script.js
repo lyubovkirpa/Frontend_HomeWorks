9dc80dd1ba69b726033aa17c9ef258b5ee1a1bff
Liubov Kyrpa <kirpa.lyubov.a@gmail.com>
2024-10-14 22:15:15 +0200

    add hw 17

--- hw_17/script.js
+++ hw_17/script.js
@@ -77,7 +77,8 @@ function uncompletedTaskHandler() {
   });
 }
 
-//TODO // Задание hw 17
+//TODO    
+ Задание hw 17
 // https://jsonplaceholder.typicode.com/todos
 // с помощью fetch взять список дел по ссылке выше, обрезать (slice) до 10-15, потом с помощью map привести элементы к нужному нам виду, и использовать этот массив для вывода в нашем проекте ту ду листа.
 
